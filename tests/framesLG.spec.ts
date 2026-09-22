c6a530eb984380c82d865aa2444227e8b7a6e48c
BalajiSaravanan <balajisaravanan210697@gmail.com>
2026-09-22 20:57:06 +0530

    nested frames implementation

--- tests/framesLG.spec.ts
+++ tests/framesLG.spec.ts
@@ -1,22 +1,22 @@
 import { expect, test } from "@playwright/test";
 
-test('frames interactions',async ({page}) => {
+test("frames interactions", async ({ page }) => {
+  await page.goto("https://leafground.com/frame.xhtml");
 
-    await page.goto('https://leafground.com/frame.xhtml')
+  const outerFrameLocator = page.frameLocator('[src="page.xhtml"]');
 
-    const outerFrameLocator = page.frameLocator('[src="page.xhtml"]')
+  const innerFrameLocator = outerFrameLocator.frameLocator('[src="framebutton.xhtml"]');
 
-    const innerFrameLocator = outerFrameLocator.frameLocator('[src="framebutton.xhtml"]')
-    
-    let beforeClick = await innerFrameLocator.locator('#Click').innerText()
-    console.log(beforeClick);
-    expect(beforeClick).toBe('Click Me')
+  let beforeClick = await innerFrameLocator.locator("#Click").innerText();
+  console.log(beforeClick);
+  expect(beforeClick).toBe("Click Me");
+  await expect(innerFrameLocator.locator("#Click")).toContainText("Click Me");
 
-    await innerFrameLocator.locator('#Click').click()
+  await innerFrameLocator.locator("#Click").click();
 
-    let afterClick = await innerFrameLocator.locator('#Click').innerText()
-    console.log(afterClick);
-    expect(afterClick).toBe('Hurray! You Clicked Me.')
-    
-    
-}) 
+  let afterClick = await innerFrameLocator.locator("#Click").innerText();
+  console.log(afterClick);
+  expect(afterClick).toBe("Hurray! You Clicked Me.");
+  await expect(innerFrameLocator.locator("#Click"))
+  .toHaveText("Hurray! You Clicked Me.",);
+});
